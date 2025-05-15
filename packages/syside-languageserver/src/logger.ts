82af5863a6089c5cc51249bfc4f45b98343df522
AdisonWisetnakon <adisonwisetnakon@gmail.com>
2025-05-16 04:33:53 +0700

    LogToDebugFile again

--- packages/syside-languageserver/src/logger.ts
+++ packages/syside-languageserver/src/logger.ts
@@ -6,11 +6,8 @@ const debugFilePath = path.join(__dirname, "LogRecord.log");
 
 // Utility to log messages into the debug file
 export function logToDebugFile(message: string): void {
-    const timestamp = new Date().toISOString(); // Add timestamp to each log
-    const logMessage = `[${timestamp}] ${message}\n`;
-
     // Append the message to the debug.log file
-    fs.appendFile(debugFilePath, logMessage, (err) => {
+    fs.appendFile(debugFilePath, message, (err) => {
         if (err) {
             console.error("Error writing to debug file:", err);
         }
